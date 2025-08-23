530f0621654505c637df79a4847724cc92c4f5b8
Yash Mastud <mastud.yash7@gmail.com>
2025-08-23 20:18:30 +0530

    url prefix search console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,6 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://yashmastud.vercel.app",
   },
-  verification: {
-    google: "DB6PN54JW7V7xICdliBBkuAU_C0UooVhNAC9mTfBpG8",
-  },
 };
 
 export default function RootLayout({
